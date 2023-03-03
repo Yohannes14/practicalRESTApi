f66d4b7173d62a6eb7f73d1c0b01bf163e2c8dbb
Yohannes <ydegu@admin.crossroads.co>
2023-03-03 17:15:47 +0300

    fetch product

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const feedRoutes= require('./routes/feed');
+const feedRoutes = require('./routes/feed');
 
 const app = express();
 
-//GET /feed/posts
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({message : message});
+});
+
+mongoose.connect(
+    'mongodb+srv://Joo1234:Joo1234@cluster1.mydli6e.mongodb.net/messages?retryWrites=true&w=majority'
+).then(() => {
+    app.listen(8080);
+
+}).catch(err => {
+    console.log(err)
+});
--- routes/feed.js
+++ routes/feed.js
@@ -1,6 +1,26 @@
-const express =require('express');
+const express = require('express');
+const { body } = require('express-validator/check');
 
 const feedController = require('../controllers/feed');
+
 const router = express.Router();
 
-router.get('/posts');
+// GET /feed/posts
+router.get('/posts', feedController.getPosts);
+
+// POST /feed/post
+router.post(
+    '/post',
+    [
+        body('title')
+            .trim()
+            .isLength({ min: 5 }),
+        body('content')
+            .trim()
+            .isLength({ min: 5}),
+    ],
+    feedController.createPost);
+
+    router.get('/post/:postId', feedController.getPost)
+
+module.exports = router;
