69b56c515f158db25b4382ca5f4858a387838ea5
Yohannes <ydegu@admin.crossroads.co>
2023-03-04 11:37:06 +0300

    logged in and create jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 
 const { validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
@@ -36,3 +38,43 @@ exports.signup = (req, res, next) => {
         });
 
 };
+
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                const error = new Error('A user with this email ould not ne found.');
+                error.statusCode = 401;
+                throw error;
+            }
+            loadedUser = user;
+            return bcrypt.compare(password, user.password);
+
+        })
+        .then(isMatch => {
+            if (!isMatch) {
+                const error = new Error('Wrong password!');
+                error.statusCode = 401;
+                throw error;
+            }
+            // creating json web token
+            const token = jwt.sign({
+                email: loadedUser.email,
+                userId: loadedUser._id.toString()
+            }, 
+            'somesupersecretsecret', 
+            { expiresIn: '1h' }
+            );
+            res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+
+}
